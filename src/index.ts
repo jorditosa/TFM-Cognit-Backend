0888baf7611c2293c913b77453b7513a16f3c249
jorditosa <jordisato88@gmail.com>
2025-01-08 15:58:19 +0100

    port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors'
 import server from './server'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3005
 
 server.listen(port, () => {
     console.log( colors.cyan.bold( `REST Api on port ${port}`))
