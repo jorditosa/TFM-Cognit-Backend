f93a04fa3f2162d56046a6a1bd29b4d6bc7dc250
jorditosa <jordisato88@gmail.com>
2025-01-12 09:49:44 +0100

    project email

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -13,10 +13,24 @@ export class AuthEmail {
             to: user.email,
             subject: "Cognit - Confirmar compte",
             html: `
-                <h1> Hola ${user.username}! </h1>
-                <p>T'has registrat a COGNIT, només queda que confirmis el teu registre per formar part de tota la comunitat, pots fer-ho clicant a </p>
-                <a href="${process.env.FRONTEND_URL}/confirm-account">confirma el teu compte</a>,
-                <p>indicant el codi de registre: ${user.token}</p>
+                <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 10px; max-width: 600px; margin: auto; border: 2px solid #086375;">
+                    <h1 style="color: #086375; text-align: center;">Hola ${user.username}!</h1>
+                    <p style="font-size: 16px; color: #333; text-align: justify;">
+                        T'has registrat a <strong style="color: #086375;">COGNIT</strong>, només queda que confirmis el teu registre per formar part de tota la comunitat. Pots fer-ho clicant a:
+                    </p>
+                    <div style="text-align: center; margin: 20px 0;">
+                        <a href="${process.env.FRONTEND_URL}/confirm-account" 
+                        style="background-color: #086375; color: #AFFC41; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-weight: bold;">
+                        Confirma el teu compte
+                        </a>
+                    </div>
+                    <p style="font-size: 16px; color: #333; text-align: center;">
+                        Indica aquest codi de registre:
+                    </p>
+                    <p style="font-size: 20px; color: #086375; font-weight: bold; text-align: center; background-color: #AFFC41; padding: 10px; border-radius: 5px; display: inline-block;">
+                        ${user.token}
+                    </p>
+                </div>
             `
         })
     }
